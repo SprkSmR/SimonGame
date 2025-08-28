ec27c4269412adb25cc1eeb966cd3d309d6a2a98
Omar Jimenez Armendáriz <chitox_ji_armen@hotmail.com>
2025-08-27 22:45:09 -0600

    Step 2 - Create A New Pattern

--- game.js
+++ game.js
@@ -1,1 +1,11 @@
-alert("funca papa");
+var buttonColours = ["red", "blue", "green", "yellow"];
+var randomChosenColour = buttonColours[nextSequence()];
+
+var gamePattern = [];
+
+gamePattern.push(randomChosenColour);
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random()*4);
+    return randomNumber;
+}
