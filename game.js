a4c122d3387add7ca71278e61d97b485396e88b9
SprkSmR <chitox_ji_armen@hotmail.com>
2025-09-02 00:00:32 -0600

    Flash tests

--- game.js
+++ game.js
@@ -9,3 +9,19 @@ function nextSequence(){
     var randomNumber = Math.floor(Math.random()*4);
     return randomNumber;
 }
+
+$.fn.flash = function(times, duration) {
+	var T = this;
+  	times = times || 3;
+ 	duration = duration || 200;
+        for ( var i=0; i < times; i++ ) {     	
+			(function() {      		
+				setTimeout(function() {
+					T.fadeOut(duration, function() { 		    							
+						T.fadeIn(duration);
+					});
+				}, i*duration*2+50);
+			})
+		(i);     
+	} 
+};
