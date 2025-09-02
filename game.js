e6533ba3a5662abc42b59e4ca4c73f84a102d0fe
Omar Jimenez Armendáriz <chitox_ji_armen@hotmail.com>
2025-09-02 12:50:52 -0600

    Flash testing success

--- game.js
+++ game.js
@@ -10,18 +10,28 @@ function nextSequence(){
     return randomNumber;
 }
 
+//Defines a new function over the generic jQuery prototype, which adds the functionality of flashing. Can pass parameters or be left blank to use defaults.
 $.fn.flash = function(times, duration) {
 	var T = this;
   	times = times || 3;
  	duration = duration || 200;
         for ( var i=0; i < times; i++ ) {     	
-			(function() {      		
+			(function(iter) {      		
 				setTimeout(function() {
-					T.fadeOut(duration, function() { 		    							
+					T.fadeOut(duration, function() {		
 						T.fadeIn(duration);
 					});
-				}, i*duration*2+50);
+				}, iter*duration*2+50);
 			})
-		(i);     
-	} 
+		(i);
+	}
+	this.prepend("<audio><source src='./sounds/"+randomChosenColour+".mp3' type='audio/mp3'/></audio>");
+	this[0].firstChild.play();
 };
+
+$(document).on("keypress", function(event){
+	if (event.originalEvent.key === "a"){
+		$("."+randomChosenColour).flash();
+	}
+});
+
